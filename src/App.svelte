65f451e095f22f2bcdbb06780c935ff3dc6bb619
eaczechova <ewa.czech@hotmail.com>
2021-12-30 21:01:54 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+</script>
+
+<Header />
+<main>IdeaBoard</main>
+<Footer />
+
+<style>
+</style>
